6a451b1c501ef3051837c9dbd5567e0e6b3090e4
andesonatasha <andesonatasha@gmail.com>
2026-04-23 10:20:08 +0300

    added novellahub codes

--- src/App.js
+++ src/App.js
@@ -1,24 +1,69 @@
-import logo from './logo.svg';
 import './App.css';
+import { BrowserRouter as Router, Routes, Route, Link } from 'react-router-dom';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import Signup from './components/Signup';
+import Signin from './components/Signin';
+// import Addproducts from './components/Addproducts';
+// import Getproducts from './components/Getproducts';
+// import Makepayment from './components/Makepayment';
+// import Notfound from './components/Notfound';
+// import Footer from './components/Footer';
 
 function App() {
   return (
+    <Router>
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+      <header>
       </header>
+      <nav className="navbar navbar-expand-lg navbar-light bg-light shadow-sm px-4 fixed-top">
+      
+      {/* Bakery Name (Left Side) */}
+      <a className="navbar-brand fw-bold text-primary brand-text" href="/">
+        NovellaHub 🦋
+      </a>
+
+      {/* Toggle Button for Mobile */}
+      <button
+        className="navbar-toggler"
+        type="button"
+        data-bs-toggle="collapse"
+        data-bs-target="#navbarContent"
+      >
+        <span className="navbar-toggler-icon"></span>
+      </button>
+
+      {/* Links (Right Side) */}
+      <div className="collapse navbar-collapse" id="navbarContent">
+        <ul className="navbar-nav ms-auto">
+          <li className="nav-item">
+            <a className="nav-link active" href="/">Home</a>
+          </li>
+
+          <li className="nav-item">
+            <a className="nav-link" href="/addproducts">Add products</a>
+          </li>
+
+          <li className="nav-item">
+            <a className="nav-link" href="/signin">Sign In</a>
+          </li>
+
+          <li className="nav-item">
+            <a className="nav-link" href="/signup">Sign Up</a>
+          </li>
+        </ul>
+      </div>
+    </nav>
+      <Routes>
+        {/* <Route path="/" element={<Getproducts />} /> */}
+        <Route path='/signup' element={<Signup />} />
+        <Route path='/signin' element={<Signin />} />
+        {/* <Route path='/addproducts' element={<Addproducts />} />
+        <Route path='/makepayment' element={<Makepayment /> } />
+        <Route path='*' element={<Notfound />} /> */}
+      </Routes>
+      {/* <Footer /> */}
     </div>
+    </Router>
   );
 }
 
---
+++ src/components/Signup.jsx
@@ -0,0 +1,126 @@
+import axios from 'axios';
+import React, { useState } from 'react'
+import { Link } from 'react-router-dom';
+
+const Signup = () => {
+  // initialize the hooks
+  const [username, setUsername] = useState("");
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const [phone, setPhone] = useState("");
+
+  // Define the three states an application will move to
+  const [loading, setLoading] = useState("");
+  const [success, setSuccess] = useState("");
+  const [error, setError] = useState("");
+
+  // Below is a function that will handle the submit action
+  const handleSubmit = async(e) =>{
+    // Below will prevent our site from reloading
+    e.preventDefault()
+
+    // Update our loading hook with a message that will be displayed to the users who are trying to register
+    setLoading("Please wait as registration is in progress...")
+
+    try{
+      // create a form data object that will enable you to capture the four details enterded on the form
+      const formdata = new FormData();
+
+      // insert the four details (username, email, password, phone) interms of key- value pairs
+      formdata.append("username", username);
+      formdata.append("email", email);
+      formdata.append("password", password);
+      formdata.append("phone", phone);
+
+      // by use of axios, we can access the method post
+      const response =await axios.post("https://tashaandeso.alwaysdata.net/api/signup", formdata)
+
+      // set back the loading to default
+      setLoading("");
+
+      // just incase everything goes on well, update the success hook with a message
+      setSuccess(response.data.message)
+
+      // clear your hooks
+      setUsername("");
+      setEmail("");
+      setPassword("");
+      setPhone("");
+
+      setTimeout(() => {
+    setSuccess("");
+  }, 5000);
+    }
+    catch(error){
+      // set the loading back to default
+      setLoading("");
+
+      // update the error hook with the message given ack from the response
+      setError(error.message)
+    }
+
+  }
+
+  return (
+    // <!-- From Uiverse.io by mi-series --> 
+<div class="container justify-content-center col-md-6 signin">
+  <div class="heading">Sign In</div>
+
+      <h5 className="text-warning">{loading}</h5>
+      <h3 className="text-success">{success}</h3>
+      <h4 className="text-danger">{error}</h4>
+      
+  <form class="form" onSubmit={handleSubmit}>
+
+     <input type="text"
+       placeholder='Enter the username'
+      className='form-control'
+      value={username}
+      onChange={(e) => setUsername(e.target.value)}
+      required /> <br />
+
+      {/* {username} */}
+    <input
+      placeholder="E-mail"
+      id="email"
+      name="email"
+      type="email"
+      class="input"
+      required
+    /> <br /> 
+
+    <input
+      placeholder="Password"
+      id="password"
+      name="password"
+      type="password"
+      class="input"
+      required
+    /> <br /> <br />
+
+    <input type="number"
+     placeholder='Enter the mobile phone number'
+     className='form-control'
+    value={phone}
+     onChange={(e) => setPhone(e.target.value)}
+    required /> <br />
+
+    {/* {phone} */}
+
+    <span class="forgot-password"><a href="#">Forgot Password ?</a></span> <br />
+    
+    <input value="Sign In" type="submit" class="login-button" />
+  </form>
+ 
+    Already  have an account? <Link to='/signin'>Signin</Link>
+
+  <span class="agreement"><a>Learn user licence agreement</a></span>
+</div>
+  )
+  
+}
+
+
+export default Signup;
+
+// Research on Axios module in reactjs
